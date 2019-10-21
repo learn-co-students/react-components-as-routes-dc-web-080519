7a02c723be089095942140f1718d0eec65204b47
SkaMateria <skamateria@gmail.com>
2019-10-21 18:00:12 -0400

    Done.

---
+++ src/routeTest.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class routeTest extends Component {
+    render() {
+        return(
+            <h1>
+                HELLO,YA FOOL!
+            </h1>
+        )
+    }
+}
+
+export default routeTest 
